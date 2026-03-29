f13118528b1820b9edca4a0aabac963fe1d10d30
jasim28v-cloud <jasim28v@gmail.com>
2026-03-29 22:10:19 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// Firebase Configuration (Modern)
 const firebaseConfig = {
     apiKey: "AIzaSyDr5RBcJ9gcHTdElXxazcEWMBoTYzC_CaU",
     authDomain: "foxe-3f428.firebaseapp.com",
@@ -7,20 +6,14 @@ const firebaseConfig = {
     storageBucket: "foxe-3f428.firebasestorage.app",
     messagingSenderId: "763563407239",
     appId: "1:763563407239:web:4e558a73bffb5e6e1e8522",
-    measurementId: "G-E7ZVVZ5HPV"
 };
 
-// Initialize Firebase (Compat version for simplicity)
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.database();
 const storage = firebase.storage();
 
-// Cloudinary Settings
 const CLOUD_NAME = 'dnmpmysk6';
 const UPLOAD_PRESET = 'rsxdfdgw';
-
-// Admin Emails (Edit this with your email)
-const ADMIN_EMAILS = ['admin@example.com']; // <--- CHANGE TO YOUR EMAIL
 
 console.log('✅ SHΔDØW Modern System Ready');
